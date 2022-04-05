5e9a85be58e053b260dc3728b2402640f1e72d02
mhrayhan <mhrayhan430@gmial.com>
2022-04-05 23:06:08 +0600

    blogs page done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
+    
     return (
-        <div>
+        <div className='notfound'>
             <h1>404</h1>
-            <h3>Page not found</h3>
+            <p>The link you followed probably broken,</p>
+            <p>or the page has been removed</p>
         </div>
     );
 };
